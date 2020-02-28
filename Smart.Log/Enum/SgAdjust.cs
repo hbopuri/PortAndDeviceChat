7736db0dfd61ca89c335696e9cc2002a4d707f91
Harsha Bopuri <hbopuri@smart-structures.com>
2020-02-28 16:18:20 +0530

    Mems test fix

---
+++ Smart.Log/Enum/SgAdjust.cs
@@ -0,0 +1,18 @@
+﻿namespace Smart.Log.Enum
+{
+    public enum SgAdjust
+    {
+        Increment,
+        Decrement,
+        Save
+    }
+    public enum AxAdjust
+    {
+        Min,
+        Mid,
+        Max,
+        HitTheNail,
+        Save,
+        ToDesiredRange
+    }
+}
